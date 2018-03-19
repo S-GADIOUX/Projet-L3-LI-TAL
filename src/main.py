45bfa56f72deb6cc43d8b87b7f42ecbdce34f145
S-GADIOUX <sebgad19@gmail.com>
2018-03-19 11:34:03 +0100

    removing bad test

--- src/main.py
+++ src/main.py
@@ -33,19 +33,16 @@ def testCode(thesau, nou):
 	print("Length : " + str(l))
 	t = (l*(l+1))/2
 	print('Final cost : ' +str(t))
-	f = 1
+	f = 0
 
 	s = ''
 	for i in range(l):
 		s+="####################\n"
 		for j in range(i+1,l):
 			s += nou[i].lexeme + " \t"+ nou[j].lexeme + " \t" + str(thesau.cosine(nou[i], nou[j], thesau.PMI)) +'\n'
-			
-			z = (i*l+(j-i))/t
-			if (z*100>f):
-				print(z*100)
-				f+=1
 			fileManager.write('tmp',s)
+			f+=1
 			s=''
+	print("Real cost :" + str(f))
 
 timeTest(testCode, thesau, nou)
